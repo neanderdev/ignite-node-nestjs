a071b2199c91ba3c956a25a64bfd1517b2fd8873
Neander de Souza <neander.faria@hotmail.com>
2023-10-17 10:04:18 -0300

    Fundamentos do Nest - Protegendo rotas com guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { Env } from '../env'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
@@ -30,5 +31,6 @@ import { Env } from '../env'
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
